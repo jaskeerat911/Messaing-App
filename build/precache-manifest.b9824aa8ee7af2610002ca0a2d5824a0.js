bfa273b8dbd441ff06bf9fcf6b2d3689684d3a1f
jaskeerat911 <jaskeerat911@gmail.com>
2022-01-25 10:10:45 +0530

    Project Build

---
+++ build/precache-manifest.b9824aa8ee7af2610002ca0a2d5824a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442b66397dc7c3dd65546452ba30853d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21424ea94e58601d9f44",
+    "url": "/static/css/2.0b0521d5.chunk.css"
+  },
+  {
+    "revision": "66d4fc0b2a229781fa93",
+    "url": "/static/css/main.2a2ac146.chunk.css"
+  },
+  {
+    "revision": "21424ea94e58601d9f44",
+    "url": "/static/js/2.670982a4.chunk.js"
+  },
+  {
+    "revision": "e7a20858d746fe9b31de80ae124cad64",
+    "url": "/static/js/2.670982a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d4fc0b2a229781fa93",
+    "url": "/static/js/main.2e7d4626.chunk.js"
+  },
+  {
+    "revision": "fa430200eae998bbeca5",
+    "url": "/static/js/runtime-main.ee4f1273.js"
+  }
+]);
